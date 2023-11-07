c510f74355948fdf725a2969ae8683e63b552621
Riley B <30447344+rbro3551@users.noreply.github.com>
2023-11-07 06:11:44 -0600

    Initial Commit

---
+++ FetchDesserts/FetchDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertsApp.swift
+//  FetchDesserts
+//
+//  Created by Riley Brookins on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
